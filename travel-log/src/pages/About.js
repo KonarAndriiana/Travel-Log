90ff0380b80758c14f6affeac180ab66dfa08500
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2025-06-15 19:19:50 +0200

    Add About, Home, and PlaceDetail pages

---
+++ travel-log/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Travel Log</h1>
+      <p>This app was created as a school project using React.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ travel-log/src/pages/PlaceDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PlaceDetail() {
+  return (
+    <div>
+      <h1>Place Detail</h1>
+      <p>Details about a specific place.</p>
+    </div>
+  );
+}
+
+export default PlaceDetail;
