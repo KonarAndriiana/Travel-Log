6d5cd4d17b5807b99140768c7fcaa645386f60ae
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2025-06-15 19:35:12 +0200

    Add AddPlace page

---
+++ travel-log/src/pages/AddPlace.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PlaceForm from '../components/PlaceForm';
+
+function AddPlace() {
+  return (
+    <div>
+      <PlaceForm />
+    </div>
+  );
+}
+
+export default AddPlace;
