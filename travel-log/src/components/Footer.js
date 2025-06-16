eb0faaf3cce611ff242c124e5ad284a6a22b3bc4
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2025-06-16 14:33:14 +0200

    Add Footer

---
+++ travel-log/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Travel Log | Created by Adulik-Vertulik
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
