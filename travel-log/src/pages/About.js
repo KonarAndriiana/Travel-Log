398da7fbddda19b789828ef0fccff43ff81a2a2a
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2025-06-16 14:11:59 +0200

    Update About page

--- travel-log/src/pages/About.js
+++ travel-log/src/pages/About.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
+    <div className="container about-page">
       <h1>About Travel Log</h1>
-      <p>This app was created as a school project using React.</p>
+      <p>
+        Travel Log je jednoduchá aplikácia, ktorú som vytvorila ako školský projekt na precvičenie práce s Reactom.
+        Cieľom bolo vytvoriť si vlastný zoznam navštívených miest – s možnosťou pridať, upraviť, vymazať alebo označiť
+        obľúbené destinácie.
+      </p>
+      <p>
+        Aplikácia využíva localStorage na ukladanie dát a obsahuje niekoľko obrazoviek: domovskú stránku, detailné
+        zobrazenie miesta, editovanie, a túto stránku o projekte.
+      </p>
+      <p>
+        Bonus: Najnavštevovanejšie mesto na svete je Bangkok 🌍
+      </p>
     </div>
   );
 }
